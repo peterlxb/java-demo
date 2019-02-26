52c91c8312d9d091ab4d213a13a49c6dc9bf5470
peterlxb <peterlxb@gmail.com>
2019-02-26 15:49:55 +0800

    初始化项目

---
+++ src/com/netease/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.netease.demo;
+
+/**
+ * Created at 2-26 2019
+ */
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
